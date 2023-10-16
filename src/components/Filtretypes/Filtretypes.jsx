5f72aa907741a519cab756d6f7b8621664e448d0
Leslie2186 <leslie.moraud@gmail.com>
2023-10-16 12:18:23 +0200

    Ajout du fitre par type, retouches css des boutons et animation pokemons au survol

--- src/components/Filtretypes/Filtretypes.jsx
+++ src/components/Filtretypes/Filtretypes.jsx
@@ -1,14 +1,28 @@
 
 import './Filtretypes.css'
 
-const Filtretypes = ({setFilter}) => {
+const Filtretypes = ({filter, setFilter}) => {
+
+    const modifFilter = (param) => {
+        setFilter(filter = param);
+    }
 
     return <div>
-        <select>
-            <option onClick={() => setFilter("all")}>Tous les types</option>
-            <option onClick={() => setFilter("plante")}>Plante</option>
-
-       </select>
+       <section id="sectionButtons">
+            <button onClick={() => modifFilter("all")}>Tous les types</button>
+            <button onClick={() => modifFilter("plante")}>Plante</button>
+            <button onClick={() => modifFilter("eau")}>Eau</button>
+            <button onClick={() => modifFilter("feu")}>Feu</button>
+            <button onClick={() => modifFilter("electrique")}>Electrique</button>
+            <button onClick={() => modifFilter("vol")}>Vol</button>
+            <button onClick={() => modifFilter("fée")}>Fée</button>
+            <button onClick={() => modifFilter("combat")}>Combat</button>
+            <button onClick={() => modifFilter("psy")}>Psy</button>
+            <button onClick={() => modifFilter("roche")}>Roche</button>
+            <button onClick={() => modifFilter("sol")}>Sol</button>
+            <button onClick={() => modifFilter("glace")}>Glace</button>
+            <button onClick={() => modifFilter("dragon")}>Dragon</button>
+       </section>
     </div>
 
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,11 @@ import logo from '../../assets/pokeball.png'
 import './Navbar.css'
 
 
-const Navbar = ({setFilter}) => {
+const Navbar = () => {
 
     return <nav>
         <img src={logo} alt="pokeball" id='logo'/>
         <h1>Pokedex</h1>
-        <select>
-            <option onClick={() => setFilter("all")}>Tous les types</option>
-            <option onClick={() => setFilter("plante")}>Plante</option>
-       </select>
     </nav>
 }
 
